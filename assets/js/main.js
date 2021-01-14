957b57e739cfaa2c59b107db3120197c1ce37778
Tyler Potts <tylerpottsdev@gmail.com>
2021-01-14 13:40:48 +0000

    init

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+	window.addEventListener('scroll', function (e) {
+		if (window.pageYOffset > 100) {
+			document.querySelector("header").classList.add('is-scrolling');
+		} else {
+			document.querySelector("header").classList.remove('is-scrolling');
+		}
+	});
+}
