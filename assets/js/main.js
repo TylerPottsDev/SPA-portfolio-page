19751a2829d696a39b1b2ac77a9e72e29b235cd5
Tyler Potts <tylerpottsdev@gmail.com>
2021-01-15 16:55:50 +0000

    HAMMY

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,12 @@ window.onload = function () {
 			document.querySelector("header").classList.remove('is-scrolling');
 		}
 	});
+
+	const menu_btn = document.querySelector('.hamburger');
+	const mobile_menu = document.querySelector('.mobile-nav');
+
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('is-active');
+	});
 }
